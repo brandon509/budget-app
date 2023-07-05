040c5be4ae3b11e11f81e21204b4e28bfac249ef
Brandon Cousins <brandon509@gmail.com>
2023-07-05 19:30:12 -0400

    fixing auth

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -67,25 +67,24 @@ module.exports = {
       return next(error)
     }
   },
-  logout: async (req,res) => {
+  logout: async (req,res,next) => {
     try {
       req.logout(() => {
-        console.log('User has logged out')
+          console.log('User has logged out')
       })
       // res.cookie("connect.sid","", {
       //   httpOnly: true,
       //   expires: new Date(0)
       // })
-      req.session.destroy((err) => {
-        if (err)
-          console.log("Error : Failed to destroy the session during logout", err);
-        req.user = null;
-        return res.status(200).json('Logged out')
-      })
+      // req.session.destroy((err) => {
+      //   if (err)
+      //     console.log("Error : Failed to destroy the session during logout", err);
+      //   req.user = null;
+      //   return res.status(200).json('Logged out')
+      // })
     } 
     catch (error) {
       console.log(error)
     }
-    
   }
 }
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const categoryController = require("../controllers/category")
+const { ensureAuth } = require("../middleware/auth")
 
-router.get('/', categoryController.getUserCategories)
+router.get('/', ensureAuth, categoryController.getUserCategories)
 router.post('/newCategory', categoryController.newCategory)
 router.put('/deleteCategory', categoryController.deleteCategory)
 router.put('/updateCategory', categoryController.updateCategory) //edit name, joint status, split, or budget
